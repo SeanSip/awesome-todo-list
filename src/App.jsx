5a892dfd5447e92802db8b86d0b3fb86a51fedc9
Sean Sipus <sean.sipus@gmail.com>
2023-05-22 10:40:48 -0400

    Add stylings

--- src/App.jsx
+++ src/App.jsx
@@ -29,13 +29,12 @@ function App() {
 
   return (
     <div className="wrapper">
-      <div className="flex flex-col text-center">
+      <div className="flex flex-col">
         <ToggleLightDarkMode className="pt-2 w-fit h-fit self-end" />
-        <h1 className="">Awesome Todo List</h1>
-        <p>One task a day, keeps your problems away!</p>
+        <h1 className="mb-5 text-center">Awesome Todo List</h1>
 
         <form onSubmit={handleSubmit}>
-          <label className="text-start" htmlFor="item">
+          <label className="text-left" htmlFor="item">
             New Task
           </label>
           <div className="flex rounded-md border-2 border-black dark:border-white bg-white">
@@ -50,23 +49,28 @@ function App() {
             </button>
           </div>
         </form>
-        <ul className="space-y-2 mt-5 flex flex-col items-start text-start w-full">
+        <ul className="space-y-4 my-16 flex flex-col items-start text-start w-full justify-between ">
           {todos.map(todo => {
             return (
               <li
                 key={todo.id}
-                className="flex justify-between w-full items-center"
+                className="flex justify-between w-full items-center border-2 border-black dark:border-white bg-lime-50/50 p-4 rounded-lg pr-12 md:pr-4"
               >
-                <label>
-                  <input type="checkbox" />
-                  {todo.title}
-                </label>
-                <button
-                  onClick={() => deleteTodo(todo.id)}
-                  className="bg-red-500 px-4 py-2 ml-2 rounded-md text-white border-2 border-black dark:border-white h-fit"
-                >
-                  Delete
-                </button>
+                <input
+                  className="sm:min-w-[30px] min-w-[20px] h-10 self-center mr-3"
+                  type="checkbox"
+                />
+                <div className="flex flex-col md:flex-row items-center  w-full py-4 justify-between overflow-auto gap-y-10">
+                  <label className="flex break-words overflow-auto">
+                    {todo.title}
+                  </label>
+                  <button
+                    onClick={() => deleteTodo(todo.id)}
+                    className="bg-red-500 px-4 py-2 rounded-md text-white border-2 border-black dark:border-white h-fit ml-3 md:w-fit w-10/12"
+                  >
+                    Delete
+                  </button>
+                </div>
               </li>
             );
           })}
